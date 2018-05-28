b479258311a8f9dfb4c7af74b0856a8cb4579345
DoYeon Oh <odyody12@gmail.com>
2018-05-28 19:44:08 +0900

    feat: install talib, tulind. rm cp.indicatorResult

--- plugins/tradingAdvisor/baseTradingMethod.js
+++ plugins/tradingAdvisor/baseTradingMethod.js
@@ -4,7 +4,6 @@ var util = require('../../core/util');
 var config = util.getConfig();
 var dirs = util.dirs();
 var log = require(dirs.core + 'log');
-var cp = require(dirs.core + 'cp');
 
 var ENV = util.gekkoEnv();
 var mode = util.gekkoMode();
@@ -252,23 +251,6 @@ Base.prototype.propogateTick = function(candle) {
   ) {
     return this.tick(this.deferredTicks.shift())
   }
-
-  // emit for UI
-  _.each(this.indicators, (indicator, name) => {
-    cp.indicatorResult({
-      name,
-      date: candle.start,
-      result: indicator.result
-    });
-  });
-
-  _.each(this.tulipIndicators, (indicator, name) => {
-    cp.indicatorResult({
-      name,
-      date: candle.start,
-      result: indicator.result
-    });
-  });
 
   // are we totally finished?
   var done = this.age === this.processedTicks;
